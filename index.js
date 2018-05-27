06906de84e40046a4da4c2bb67f6a3cfa2292a31
Joshua D Borup <joshborup@gmail.com>
2018-05-27 15:19:18 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
 const express = require('express');
 const session = require('express-session');
-const passport = require('passport');
-const request = require('request');
-const strategy = require(`${__dirname}/strategy.js`);
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dot-env').config();
+
+app.use(bodyParser.json());
+
 
 const app = express();
 app.use( session({
-  secret: '@nyth!ng y0u w@nT',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }));
-app.use( passport.initialize() );
-app.use( passport.session() );
-passport.use( strategy );
 
-passport.serializeUser( (user, done) => {
-  const { _json } = user;
-  done(null, { clientID: _json.clientID, email: _json.email, name: _json.name, followers: _json.followers_url });
-});
+app.get('/auth/callback', (req, res) => {
+  
+  let payLoad = {
+    
+    client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+    client_secret: process.env.REACT_APP_AUTH0_CLIENT_SECRET,
+    code: req.query.code,
+    grant_type: 'authorization_code',
+    redirect_uri: `http://${req.headers.host}/auth/callback`
 
-passport.deserializeUser( (obj, done) => {
-  done(null, obj);
-});
-
-app.get( '/login',
-  passport.authenticate('auth0', 
-    { successRedirect: '/followers', failureRedirect: '/login', failureFlash: true, connection: 'github' }
-  )
-);
-
-app.get('/followers', ( req, res, next ) => {
-  if ( req.user ) {
-    const FollowersRequest = {
-      url: req.user.followers,
-      headers: {
-        'User-Agent': req.user.clientID
-      }
-    };
-
-    request(FollowersRequest, ( error, response, body ) => {
-      res.status(200).send(body);
-    });
-  } else {
-    res.redirect('/login');
   }
-});
+
+
+})
+
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
