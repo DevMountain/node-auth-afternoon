c20b6e066010ae8113477e9c20e1b1394fcfa50c
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-05-29 02:15:07 -0700

    consolidated all auth0 token exchanges to the /callback route

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use( session({
 }));
 
 app.get('/callback', (req, res) => {
-
+  console.log('hit')
   let payload = {
     client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
     code: req.query.code,
@@ -32,43 +32,73 @@ app.get('/callback', (req, res) => {
     return axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo/?access_token=${accessToken}`) 
   }
 
-  function setUserToSession(userInfoResponse){
+  function setUserToSessionGetAuthAccessToken(userInfoResponse){
     req.session.user = userInfoResponse.data
-    res.send(req.session.user)
-  }
-
-  exchangeCodeForAccessToken()
-  .then(accessTokenResponse => exchangeAccessTokenForUserInfo(accessTokenResponse))
-  .then(userInfoResponse => setUserToSession(userInfoResponse));        
-})
-
-app.get('/gitaccess', (req, res) => {
-  options = { 
-    method: 'POST',
-    url: 'https://joshborup.auth0.com/oauth/token',
-    headers: { 
-        'content-type': 'application/json'
-    },
-    body: {
+    options = {
+      body: {
         grant_type: 'client_credentials',
         client_id: process.env.AUTH0_API_CLIENT_ID,
         client_secret: process.env.AUTH0_API_CLIENT_SECRET,
         audience: 'https://joshborup.auth0.com/api/v2/'
-    },
-    json: true 
-  }
-  axios.post('https://joshborup.auth0.com/oauth/token', options.body).then(resp => {
-    req.session.access_token = resp.data.access_token
-    let options = {
-    headers: {authorization: `Bearer ${req.session.access_token}`}
+      },
+      json: true 
     }
-    axios.get(`https://joshborup.auth0.com/api/v2/users/${req.session.user.sub}`, options).then(resp => {
-        console.log(resp.data.identities)
-        req.session.access_token = resp.data.identities[0].access_token
-        res.send(req.session.access_token)
-    }).catch(err => console.log(err))
-  })
+    return axios.post('https://joshborup.auth0.com/oauth/token', options.body)
+  }
+
+  function getGitAccessToken(AuthAccessTokenResponse){
+    req.session.access_token = AuthAccessTokenResponse.data.access_token
+    let options = {
+      headers: {authorization: `Bearer ${req.session.access_token}`}
+    }
+    return axios.get(`https://joshborup.auth0.com/api/v2/users/${req.session.user.sub}`, options)
+  }
+
+  function setGitTokenToSessions(gitAccessToken){
+    console.log(gitAccessToken.data.identities)
+    req.session.access_token = gitAccessToken.data.identities[0].access_token
+    res.redirect('http://localhost:3000')
+  }
+
+  exchangeCodeForAccessToken()
+  .then(accessTokenResponse => exchangeAccessTokenForUserInfo(accessTokenResponse))
+  .then(userInfoResponse => setUserToSessionGetAuthAccessToken(userInfoResponse))
+  .then(AuthAccessTokenResponse => getGitAccessToken(AuthAccessTokenResponse))
+  .then(gitAccessToken => setGitTokenToSessions(gitAccessToken))
+  .catch(err =>  console.log(err))      
 })
+
+app.get('/user-data', (req, res) => {
+  res.status(200).json(req.session.user)
+})
+
+// app.get('/gitaccess', (req, res) => {
+//   options = { 
+//     method: 'POST',
+//     url: 'https://joshborup.auth0.com/oauth/token',
+//     headers: { 
+//         'content-type': 'application/json'
+//     },
+//     body: {
+//         grant_type: 'client_credentials',
+//         client_id: process.env.AUTH0_API_CLIENT_ID,
+//         client_secret: process.env.AUTH0_API_CLIENT_SECRET,
+//         audience: 'https://joshborup.auth0.com/api/v2/'
+//     },
+//     json: true 
+//   }
+//   axios.post('https://joshborup.auth0.com/oauth/token', options.body).then(resp => {
+//     req.session.access_token = resp.data.access_token
+//     let options = {
+//     headers: {authorization: `Bearer ${req.session.access_token}`}
+//     }
+//     axios.get(`https://joshborup.auth0.com/api/v2/users/${req.session.user.sub}`, options).then(resp => {
+//         console.log(resp.data.identities)
+//         req.session.access_token = resp.data.identities[0].access_token
+//         res.send(req.session.access_token)
+//     }).catch(err => console.log(err))
+//   })
+// })
 
 app.get('/star', (req, res) => {
   axios.put(`https://api.github.com/user/starred/joshborup/node-auth-afternoon?access_token=${req.session.access_token}`).then(response => {
@@ -94,5 +124,5 @@ app.get('/logout', (req, res) => {
   res.send('logged out');
 })
 
-const port = 3000;
+const port = 4000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
--- src/App.js
+++ src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      user: '',
+      starred: 'star'
+    }
+  }
+
+  componentDidMount(){
+    axios.get('/user-data').then(user => {
+      console.log(user);
+      this.setState({
+        user: user.data
+      })
+    })
+  }
+
+  login = () => {
+    const redirectUri = encodeURIComponent(`http://localhost:4000/callback`);
+    window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectUri}&response_type=code`
+  }
+
+  star = () => {
+    if(this.state.starred === 'star'){
+      this.setState({
+        starred: 'unstar'
+      })
+      return axios.get('/star')
+    }else {
+      this.setState({
+        starred: 'star'
+      })
+      return axios.get('/unstar')
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {
+            this.state.user ?
+            <div>
+              <p>{this.state.user.name}</p>
+              <button onClick={this.star}>{this.state.starred}</button>
+            </div>
+            :
+            <button onClick={this.login}>login</button>
+          }
+        </div>
       </div>
     );
   }
