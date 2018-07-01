b483d7b54a6c20f3dac27413811cdc153b36e89a
Tyler Collier <tyler.collier+github@devmounta.in>
2018-07-01 15:38:17 -0700

    Add solution for new Auth0 teaching

--- server/index.js
+++ server/index.js
@@ -1,20 +1,102 @@
 const express = require('express');
+const session = require('express-session');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
 const app = express();
+app.use(bodyParser.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
 
 app.get('/callback', (req, res) => {
+  exchangeCodeForAccessToken()
+    .then(exchangeAccessTokenForUserInfo)
+    .then(fetchAuth0AccessToken)
+    .then(fetchGitHubAccessToken)
+    .then(setGitTokenToSession)
+    .catch(error => {
+      console.log('Server error', error);
+      res.status(500).send('An error occurred on the server. Check the terminal.');
+    });
 
-  // Code below
+  function exchangeCodeForAccessToken() {
+    const payload = {
+      client_id: process.env.REACT_APP_AUTH0_CLIENT_ID,
+      client_secret: process.env.AUTH0_CLIENT_SECRET,
+      code: req.query.code,
+      grant_type: 'authorization_code',
+      redirect_uri: `http://${req.headers.host}/callback`
+    };
 
-})
+    return axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload);
+  }
+
+  function exchangeAccessTokenForUserInfo(accessTokenResponse) {
+    const accessToken = accessTokenResponse.data.access_token;
+    return axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/userinfo?access_token=${accessToken}`); 
+  }
+
+  function fetchAuth0AccessToken(userInfoResponse) {
+    req.session.user = userInfoResponse.data;
+    
+    const payload = {
+      grant_type: 'client_credentials',
+      client_id: process.env.AUTH0_API_CLIENT_ID,
+      client_secret: process.env.AUTH0_API_CLIENT_SECRET,
+      audience: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/`
+    };
+  
+    return axios.post(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/oauth/token`, payload);
+  }
+
+  function fetchGitHubAccessToken(auth0AccessTokenResponse) {
+    const options = {
+      headers: {
+        authorization: `Bearer ${auth0AccessTokenResponse.data.access_token}`
+      }
+    };
+    return axios.get(`https://${process.env.REACT_APP_AUTH0_DOMAIN}/api/v2/users/${req.session.user.sub}`, options);
+  }
+
+  function setGitTokenToSession(gitHubAccessTokenResponse) {
+    const githubIdentity = gitHubAccessTokenResponse.data.identities[0];
+    req.session.gitHubAccessToken = githubIdentity.access_token;
+    res.redirect('/');
+  }
+});
+
+app.put('/api/star', (req, res) => {
+  const { gitUser, gitRepo } = req.query;
+  axios.put(`https://api.github.com/user/starred/${gitUser}/${gitRepo}?access_token=${req.session.gitHubAccessToken}`).then(() => {
+    res.end();
+  }).catch(err => {
+    console.log('Error starring repo', err);
+    res.status(500).json({ message: 'An error occurred on the server. Check the terminal.' });
+  });
+});
+
+app.delete('/api/star', (req, res) => {
+  const { gitUser, gitRepo } = req.query;
+  axios.delete(`https://api.github.com/user/starred/${gitUser}/${gitRepo}?access_token=${req.session.gitHubAccessToken}`).then(() => {
+    res.end()
+  }).catch(err => {
+    console.log('Error unstarring repo', err);
+    res.status(500).json({ message: 'An error occurred on the server. Check the terminal.' });
+  });
+});
 
 app.get('/api/user-data', (req, res) => {
   res.status(200).json(req.session.user)
-})
+});
 
-app.get('/api/logout', (req, res) => {
+app.post('/api/logout', (req, res) => {
   req.session.destroy();
   res.send('logged out');
-})
+});
 
 const port = 4000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
--- src/App.js
+++ src/App.js
@@ -5,62 +5,61 @@ import './App.css';
 
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
       user: '',
-      starred: 'star',
-      gitRepo:'node-auth-afternoon',
-      gitUser:'',
-      message:'Please Login'
-    }
+      starred: false,
+      gitRepo: 'node-auth-afternoon',
+      gitUser: '',
+      message: 'Please Login',
+      errorMessage: ''
+    };
   }
 
   componentDidMount(){
     axios.get('/api/user-data').then(user => {
-      console.log(user);
       this.setState({
         user: user.data,
         gitUser: user.data.nickname
-      })
-    })
+      });
+    });
   }
 
   login = () => {
-    alert('set up your login function here')
+    const redirectUri = encodeURIComponent(`${window.location.origin}/callback`);
+  
+    window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectUri}&response_type=code`;
   }
 
   logout = () => {
-    axios.get('/api/logout').then(response => {
+    axios.post('/api/logout').then(response => {
       this.setState({
         message: response.data,
         user:''
-      })
-    })
+      });
+    });
   }
 
   changeHandler = (key, value) => {
     this.setState({
       [key]: value,
-      starred: 'star'
-    })
+      starred: false
+    });
   }
 
   star = () => {
-    if(this.state.starred === 'star'){
-      this.setState({
-        starred: 'unstar'
-      })
-      return axios.get(`/api/star?gitUser=${this.state.gitUser}&gitRepo=${this.state.gitRepo}`)
-    }else {
-      this.setState({
-        starred: 'star'
-      })
-      return axios.get(`/api/unstar?gitUser=${this.state.gitUser}&gitRepo=${this.state.gitRepo}`)
+    if (this.state.starred) {
+      return axios.delete(`/api/star?gitUser=${this.state.gitUser}&gitRepo=${this.state.gitRepo}`)
+        .then(() => this.setState({ starred: false }))
+        .catch(error => this.setState({ errorMessage: error.message }));
+    } else {
+      return axios.put(`/api/star?gitUser=${this.state.gitUser}&gitRepo=${this.state.gitRepo}`)
+        .then(() => this.setState({ starred: true }))
+        .catch(error => this.setState({ errorMessage: error.message }));
     }
   }
 
   render() {
-    console.log(this.state.gitRepo);
     return (
       <div className="App">
         <div>
@@ -77,9 +76,10 @@ class App extends Component {
               <input onChange={(e)=> this.changeHandler(e.target.name, e.target.value)} name='gitRepo' placeholder='Repo to star' value={this.state.gitRepo} />
 
               <div>
-                <button onClick={this.star}>{this.state.starred}</button>
+                <button onClick={this.star}>{this.state.starred ? 'unstar' : 'star'}</button>
                 <button onClick={this.logout}>logout</button>
               </div>
+              <div>{this.state.errorMessage}</div>
             </div>
             :
             <div>
