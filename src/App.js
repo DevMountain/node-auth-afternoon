154cd796399634fe6a3e5216bc315e27b4035c38
Tyler Collier <tyler.collier+github@devmounta.in>
2018-07-01 16:02:10 -0700

    Clean up front-end

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       user: '',
       starred: false,
       gitRepo: 'node-auth-afternoon',
       gitUser: '',
-      message: 'Please Login',
       errorMessage: ''
     };
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
+    this.star = this.star.bind(this);
   }
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('/api/user-data').then(user => {
       this.setState({
         user: user.data,
@@ -25,13 +26,13 @@ class App extends Component {
     });
   }
 
-  login = () => {
+  login() {
     const redirectUri = encodeURIComponent(`${window.location.origin}/callback`);
   
     window.location = `https://${process.env.REACT_APP_AUTH0_DOMAIN}/authorize?client_id=${process.env.REACT_APP_AUTH0_CLIENT_ID}&scope=openid%20profile%20email&redirect_uri=${redirectUri}&response_type=code`;
   }
 
-  logout = () => {
+  logout() {
     axios.post('/api/logout').then(response => {
       this.setState({
         message: response.data,
@@ -40,14 +41,7 @@ class App extends Component {
     });
   }
 
-  changeHandler = (key, value) => {
-    this.setState({
-      [key]: value,
-      starred: false
-    });
-  }
-
-  star = () => {
+  star() {
     if (this.state.starred) {
       return axios.delete(`/api/star?gitUser=${this.state.gitUser}&gitRepo=${this.state.gitRepo}`)
         .then(() => this.setState({ starred: false }))
@@ -63,29 +57,27 @@ class App extends Component {
     return (
       <div className="App">
         <div>
-          {
-            this.state.user ?
-            <div>
-              <div className='user-image-container'>
-                <img src={this.state.user.picture}/>
+          {this.state.user
+            ? <div>
+                <div className='user-image-container'>
+                  <img src={this.state.user.picture}/>
+                </div>
+
+                <p>{this.state.user.name}</p>
+
+                <input onChange={e => this.setState({ gitUser: e.target.value })} placeholder='Repo Owner' value={this.state.gitUser}/>
+                <input onChange={e => this.setState({ gitRepo: e.target.value })} placeholder='Repo to star' value={this.state.gitRepo} />
+
+                <div>
+                  <button onClick={this.star}>{this.state.starred ? 'Unstar' : 'Add star'}</button>
+                  <button onClick={this.logout}>logout</button>
+                </div>
+                <div>{this.state.errorMessage}</div>
               </div>
-
-              <p>{this.state.user.name}</p>
-
-              <input onChange={(e)=> this.changeHandler(e.target.name, e.target.value)}  name='gitUser' placeholder='Repo Owner' value={this.state.gitUser}/>
-              <input onChange={(e)=> this.changeHandler(e.target.name, e.target.value)} name='gitRepo' placeholder='Repo to star' value={this.state.gitRepo} />
-
-              <div>
-                <button onClick={this.star}>{this.state.starred ? 'unstar' : 'star'}</button>
-                <button onClick={this.logout}>logout</button>
+            : <div>
+                <p>Please login</p>
+                <button onClick={this.login}>Login</button>
               </div>
-              <div>{this.state.errorMessage}</div>
-            </div>
-            :
-            <div>
-              <p>{this.state.message}</p>
-              <button onClick={this.login}>login</button>
-            </div>
           }
         </div>
       </div>
