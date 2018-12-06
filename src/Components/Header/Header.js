082f3c3d4434e0624121d125564cbe935fd5cdab
Travis Allen <travisallen6@gmail.com>
2018-12-06 14:44:15 -0700

    Changed starting code

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Header.css';
-import axios from 'axios';
 
 export default class Header extends Component {
   constructor() {
@@ -29,37 +28,15 @@ export default class Header extends Component {
   }
 
   login() {
-    const { username, password } = this.state;
-    axios
-      .post('/auth/login', { username, password })
-      .then(user => {
-        this.props.updateUser(user.data);
-        this.setState({ username: '', password: '' });
-      })
-      .catch(err => alert(err.response.request.response));
+    // create POST request to /auth/login endpoint
   }
 
   register() {
-    const { username, password, isAdmin } = this.state;
-    axios
-      .post('/auth/register', { username, password, isAdmin })
-      .then(user => {
-        this.setState({ username: '', password: '' });
-        this.props.updateUser(user.data);
-      })
-      .catch(err => {
-        this.setState({ username: '', password: '' });
-        alert(err.response.request.response);
-      });
+    // create POST request to /auth/register endpoint
   }
 
   logout() {
-    axios
-      .get('/auth/logout')
-      .then(() => {
-        this.props.updateUser({});
-      })
-      .catch(err => console.log(err));
+    // create POST request to /auth/register endpoint
   }
 
   render() {
